876d84637305040bfc0fc0ede3ab3c554c4af988
jobchoi <sedcreta12@gmail.com>
2018-02-26 17:01:00 +0900

    정리후 다시 작성, tts를 사용할 함수로 문자열 전달확인

--- calendar/quick_google/main.cpp
+++ calendar/quick_google/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
 #include <Python.h>
 
+#define UNSET   0
+#define SET     1
+
 using namespace std;
+
+void event_tts(char* event_list);
 
 
 int main (int argc, char *const argv[])
@@ -10,16 +16,16 @@ int main (int argc, char *const argv[])
 	PyObject *pget_list;
     PyObject *pModule, *pName;
     string eventString;
+    int pyRun = SET;
 
-    char* test;
-
+    char* event_list = NULL;
 
     Py_Initialize();
 
 	/* start set path */
-	PyObject *sys = PyImport_ImportModule("sys");
-	PyObject *path = PyObject_GetAttrString(sys, "path");
-	PyList_Append(path, PyString_FromString("."));
+  	PyObject *sys = PyImport_ImportModule("sys");
+  	PyObject *path = PyObject_GetAttrString(sys, "path");
+  	PyList_Append(path, PyString_FromString("."));
 
 	/* get python script */
   	pName = PyString_FromString("quickstart");
@@ -27,7 +33,7 @@ int main (int argc, char *const argv[])
 
 	if (pModule != NULL)
 	{
-        pget_list	= PyObject_GetAttrString(pModule, "get_list");	
+        pget_list = PyObject_GetAttrString(pModule, "get_list");	
 
         if (!(pget_list && PyCallable_Check(pget_list)) )
         {
@@ -41,7 +47,7 @@ int main (int argc, char *const argv[])
 
         /* python module implementaion */
         puts("============================================================");
-        puts("\t\t\t\t cpp start");
+        puts("\t\t\tmain.cpp start                                          ");
         puts("============================================================");
         if(!PyString_AsString(g_pArgs))
         // No event
@@ -49,18 +55,22 @@ int main (int argc, char *const argv[])
         else 
         // One or more events
            // printf("%s\n",PyString_AsString(g_pArgs));
-            test = PyString_AsString(g_pArgs);
-        if(!test)
-            exit(0);
-        printf("string test :\n %s\n",test);
-        puts("============================================================");
-        puts("\t\t\t\t cpp finish");
-        puts("============================================================");
+            //  return event - python -> c++
+            event_list = PyString_AsString(g_pArgs);
+        if(!event_list)
+        {
+            cout <<"event_list NULL " << event_list <<endl;
+            return(-1);
+        }
+        else{
+            printf("python -> cpp : %s\n",event_list);
+            puts("");
+        } 
 	}
 	else
 	{
 		PyErr_Print();
-		std::cout << "Failed to load 'calendar??'" << std::endl;
+		cout << "Failed to load 'calendar??'" << endl;
 		return 1;
 	}
 	
@@ -74,11 +84,38 @@ int main (int argc, char *const argv[])
     printf("decref 3 \n");
 
     Py_Finalize();
-
     printf("cpp End\n");
+
+    switch(pyRun){
+        case UNSET:
+            printf("case UNSET : %d\n",pyRun);
+            break;
+        case SET:
+            event_tts(event_list);
+            break;
+        default:
+            printf("case default : %d\n",pyRun);
+            break;
+    }
+
 
 	return 0;
 }
 
+
+void event_tts(char* event_list)
+{
+    /* get python script */
+    printf("event_tts in : %s\n",event_list);
+
+//    pModule = PyImport_ImportModule("Pymodule");
+//    assert(pModule != NULL);
+//    klass = PyObject_GetAttrString(module, "math");
+//    instance = PyInstance_New(aaa,NULL,NULL);
+//    result = PyObject_CallMethod(instance,"","()",  );
+//    printf("result : %lf\n",PyInt_AsLong(result);
+
+    return ;
+}
 
 
