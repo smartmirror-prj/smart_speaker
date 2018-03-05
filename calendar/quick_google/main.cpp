4b30175316fdff70a3b455a607e40affd3d4b995
jobchoi <sedcreta12@gmail.com>
2018-03-05 16:56:49 +0900

    python class 까지 접근, method에서 값전달 진행중

--- calendar/quick_google/main.cpp
+++ calendar/quick_google/main.cpp
@@ -51,78 +51,83 @@ int main (int argc, char *const argv[])
         puts("============================================================");
         puts("\t\t\tmain.cpp start                                          ");
         puts("============================================================");
-        if(!PyString_AsString(g_pArgs)) // No event
-            printf("No upcoming events found\n");
-        else {// One or more events
-            printf("%s\n",PyString_AsString(g_pArgs));
-            event_list = PyString_AsString(g_pArgs);
+            if(!PyString_AsString(g_pArgs)) // No event
+                printf("No upcoming events found\n");
+            else {// One or more events
+                printf("%s\n",PyString_AsString(g_pArgs));
+                event_list = PyString_AsString(g_pArgs);
+            }
+            if(!event_list)
+            {
+                cout <<"event_list NULL " << event_list <<endl;
+                return(-1);
+            }
+            else printf("python -> cpp : %s\n",event_list);
         }
-        if(!event_list)
+        else
         {
-            cout <<"event_list NULL " << event_list <<endl;
-            return(-1);
+            PyErr_Print();
+            cout << "Failed to load 'calendar??'" << endl;
+            return 1;
         }
-        else printf("python -> cpp : %s\n",event_list);
-	}
-	else
-	{
-		PyErr_Print();
-		cout << "Failed to load 'calendar??'" << endl;
-		return 1;
-	}
-	
-    Py_DECREF(pget_list);
-    printf("decref 0 \n");
-    Py_DECREF(pModule);
-    printf("decref 1 \n");
-    Py_DECREF(pName);
-    printf("decref 2 \n");
-    Py_DECREF(g_pArgs);
-    printf("decref 3 \n");
+        
+        Py_DECREF(pget_list);
+        printf("decref 0 \n");
+        Py_DECREF(pModule);
+        printf("decref 1 \n");
+        Py_DECREF(pName);
+        printf("decref 2 \n");
+        Py_DECREF(g_pArgs);
+        printf("decref 3 \n");
 
-    Py_Finalize();
-    printf("cpp End\n");
+        Py_Finalize();
+        printf("cpp End\n");
 
-    switch(pyRun){
-        case UNSET:
-            printf("case UNSET : %d\n",pyRun);
-            break;
-        case SET:
-            result = event_tts(event_list);
-            printf("function event_tts resut : %d \n",result);
-            break;
-        default:
-            printf("case default : %d\n",pyRun);
-            break;
+        switch(pyRun){
+            case UNSET:
+                printf("case UNSET : %d\n",pyRun);
+                break;
+            case SET:
+                result = event_tts(event_list);
+                printf("function event_tts resut : %d \n",result);
+                break;
+            default:
+                printf("case default : %d\n",pyRun);
+                break;
+        }
+        return 0;
     }
-	return 0;
-}
 
 
-int event_tts(char* event_list)
-{
-    PyObject *pModule, *pName;
-    PyObject *pttsNaver, *test, *pInstance;
-
-    printf("event_tts in : %s\n",event_list);
-
-    Py_Initialize();
-
-	/* start set path */
-  	PyObject *sys = PyImport_ImportModule("sys");
-  	PyObject *path = PyObject_GetAttrString(sys, "path");
-  	PyList_Append(path, PyString_FromString("."));
-
-  	pName = PyString_FromString("tts_naver");
-	pModule = PyImport_Import(pName);
-	if (pModule != NULL)
+    int event_tts(char* event_list)
     {
-        puts("----");
-    }
+        PyObject *pModule, *pName;
+        PyObject *pttsNaver, *test, *pClass, *pInstance, *result;
 
+        printf("event_tts in : %s\n",event_list);
+
+        Py_Initialize();
+
+        /* start set path */
+        PyObject *sys = PyImport_ImportModule("sys");
+        PyObject *path = PyObject_GetAttrString(sys, "path");
+        PyList_Append(path, PyString_FromString("."));
+
+        pName = PyString_FromString("tts_naver");
+        assert(pName != NULL);
+        pModule = PyImport_Import(pName);
+        assert(pName != NULL);
+        pClass = PyObject_GetAttrString(pModule, "tts_class");
+        assert(pClass != NULL);
+        pInstance = PyInstance_New(pClass,NULL,NULL);
+        assert(pInstance != NULL);
+
+        //result = PyObject_CallMethod(pInstance,"test","(s)");
+        result = PyObject_CallMethod(pInstance,"test","(s)",event_list);
+        assert(result != NULL);
+    printf("result : %s\n",result);
 
     Py_Finalize();
-
     return 1;
 }
 
