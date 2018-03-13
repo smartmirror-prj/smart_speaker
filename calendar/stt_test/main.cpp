f6658a5f94bfa18148f4a024e6a6cc33988882a6
jobchoi <sedcreta12@gmail.com>
2018-03-13 17:53:22 +0900

    hold

--- calendar/stt_test/main.cpp
+++ calendar/stt_test/main.cpp
@@ -25,19 +25,20 @@ int SttFunc(){
 	PyObject *pName, *pModule;	    //for load python script
 	PyObject *pgoogle_stt;	        //for python functions
 	PyObject *g_pArgs, *g_pValue;	//for get/set python function parameters
+    PyObject *pstt_func;
 	Py_Initialize();
 	//start set path
 	PyObject *sys = PyImport_ImportModule("sys");
 	PyObject *path = PyObject_GetAttrString(sys, "path");
 	PyList_Append(path, PyString_FromString("."));
 
+	/* get python script */
     pName = PyString_FromString("stt");
     assert(pName != NULL);
     pModule = PyImport_Import(pName);
     assert(pModule != NULL);
     pgoogle_stt = PyObject_GetAttrString(pModule, "google_stt");
     assert(pgoogle_stt != NULL);
-
 
     if (!(pgoogle_stt && PyCallable_Check(pgoogle_stt)) )
     {
